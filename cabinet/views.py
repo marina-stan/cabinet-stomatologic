57b91776c708abcfe42036b29d8c503412f53a10
marina.stan <marina.ghionea@gmail.com>
2021-06-23 23:00:38 +0300

    adaugare asistenti.html, modificari medici.html si contul-meu.html

--- cabinet/views.py
+++ cabinet/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 import operator
+from managementcabinet.models import Person
 from managementcabinet.models import Medic
-from django.contrib.auth.models import User
+from managementcabinet.models import Asistent
+from managementcabinet.models import Pacient
+from django.contrib.auth.models import User, Group
 from django.contrib import auth
 from django.contrib.auth import authenticate, login
 from cabinet.forms import contactformemail
@@ -27,14 +30,31 @@ def contulMeu(request):
 def medici(request):
     return render(request, 'medici.html')
 
+def asistenti(request):
+    return render(request, 'asistenti.html')
+
+def is_medic(user):
+    return user.groups.filter(name='Medici').exists()
+
+def is_asistent(user):
+    return user.groups.filter(name='Asistenti').exists()
+
+def is_pacient(user):
+    return user.groups.filter(name='Pacienti').exists()
+
 def conectare(request):
     if request.method == 'POST':
         user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
         if user is not None:
             auth.login(request, user)
-            return render(request, 'contul-meu.html')
+            if is_medic(user):
+                return render(request, 'medici.html')
+            elif is_asistent(user):
+                return render(request, 'asistenti.html')
+            elif is_pacient(user):
+                return render(request, 'contul-meu.html')
         else:
-            return render(request, 'conectare.html', {'error':'Adresa de email sau parola sunt gresite!' })
+            return render(request, 'conectare.html', {'error':'*Adresa de email sau parola sunt gresite!' })
     else:
         return render(request, 'conectare.html')
 
